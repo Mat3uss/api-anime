c0f1170128e5e637a2309f8f41391b67deade2c8
Matheus Zanoni <matheuzzanoni@gmail.com>
2023-10-11 11:50:58 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
     const searchForm = document.getElementById("search_form");
     const animeResults = document.getElementById("anime-results");
-  
+
     if (searchForm) {
         searchForm.addEventListener("submit", function (e) {
             e.preventDefault();
-            const animeTitle = document.getElementById("search").value;
-            if (animeTitle) {
-                searchAnime(animeTitle);
+            const animeName = document.getElementById("search").value;
+            if (animeName) {
+                searchAnime(animeName);
             }
         });
-  
-        const searchAnime = async (title) => {
-            const apiUrl = `https://api.jikan.moe/v4/anime/{id}`;
-  
-            fetch(apiUrl)
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error("Network response was not ok");
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    displayAnimeResults(data);
-                })
-                .catch((error) => {
-                    animeResults.innerHTML = "Error fetching anime data.";
-                    console.error("Network response was not ok");
-                });
-        }
-  
-        function displayAnimeResults(data) {
-            animeResults.innerHTML = "";
-  
-            if (data && data.results) {
-                data.results.forEach((result) => {
-                    const title = result.title || "Title not available";
-                    const description = result.synopsis || "Description not available";
-                    const coverImage = result.image_url || "https://via.placeholder.com/200";
-  
-                    const animeCard = document.createElement("div");
-                    animeCard.classList.add("card");
-                    animeCard.innerHTML = `
-                        <div class="card-image">
-                            <img src="${coverImage}" alt="${title}">
-                        </div>
-                        <div class="card-content">
-                            <span class="card-title">${title}</span>
-                            <p>${description}</p>
-                        </div>
-                    `;
-  
-                    animeResults.appendChild(animeCard);
-                });
+    }
+
+    const searchAnime = async (name) => {
+        const apiUrl = `https://api.jikan.moe/v4/anime?q=${name}&sfw`;
+
+        try {
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
             }
+            const data = await response.json();
+            displayAnimeResults(data);
+        } catch (error) {
+            animeResults.innerHTML = "Error fetching anime data.";
+            console.error(error);
         }
     }
-  });
+
+    function displayAnimeResults(data) {
+        animeResults.innerHTML = "";
+    
+        if (data && data.data) {
+            const sortedData = data.data.sort((a, b) => a.title.localeCompare(b.title));
+    
+            sortedData.forEach((anime) => {
+                const title = anime.title || "Title not available";
+                const coverImage = anime.images?.jpg?.image_url || "https://via.placeholder.com/200";
+    
+                const animeCard = document.createElement("li"); // Use uma lista não ordenada (ul) com itens de lista (li) para cada anime
+                animeCard.classList.add("anime-list-item");
+                animeCard.innerHTML = `
+                    <h2 class="anime-title">${title}</h2>
+                    <a href="${anime.url}" target="_blank" class="mal-link">
+                        <img src="${coverImage}" alt="${title}">
+                    </a>
+                `;
+    
+                animeResults.appendChild(animeCard);
+    
+                // Adicionar um link para o MyAnimeList com o atributo "href" e a imagem dentro do link
+            });
+        } else {
+            animeResults.innerHTML = "Nenhum resultado encontrado.";
+        }
+    }
+});
